01a0def23b2341e0320c677a7d8d65c198f42e9c
andypinta <andydjpro14@gmail.com>
2024-06-03 21:55:09 -0300

    Tarea #13 - Añadi inputs en base a la actualizacion de la base de datos y agregue labels para separa a datos infromante de datos mascotas encontradas

--- app.py
+++ app.py
@@ -10,9 +10,12 @@ def home():
 def about():
     return render_template('about.html')
 
+
 @app.route("/contact")
 def contact():
     return render_template('contact.html')
+
+
 @app.route("/mapa")
 def mapa():
     return "mapa"
