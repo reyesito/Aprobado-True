298c68adeef3e4efe7c6cd0da5969a9a8ce9f0ee
Tarkus <lmsotelo@fi.uba.ar>
2024-06-05 21:26:10 -0300

    correccion de funciones

--- api_bdd/duenios_api.py
+++ api_bdd/duenios_api.py
@@ -4,8 +4,7 @@ from sqlalchemy import create_engine, text
 from sqlalchemy.exc import SQLAlchemyError
 
 app = Flask(__name__)
-engine = create_engine("mysql+mysqlconnector://root:12345@localhost/lost_pets_db")
-
+engine = create_engine("mysql+mysqlconnector://root@localhost/ids")
 
 @app.route('/duenios', methods=['GET'])
 def obtener_duenios():
@@ -13,100 +12,100 @@ def obtener_duenios():
     query = "SELECT * FROM duenios;"
     try:
         result = conn.execute(text(query))
-        data = []
-        for row in result:
-            entity = {
-                'id_duenio': row.id_duenio,
-                'nombre': row.nombre,
-                'mail': row.mail,
-                'telefono': row.telefono,
-                'barrio': row.barrio
-            }
-            data.append(entity)
+        conn.close()
     except SQLAlchemyError as err:
         return jsonify(str(err.__cause__)), 500
-    finally:
-        conn.close()
+
+    data = []
+    for row in result:
+        entity = {
+            'id_duenio': row.id_duenio,
+            'nombre': row.nombre,
+            'mail': row.mail,
+            'telefono': row.telefono,
+            'barrio': row.barrio
+        }
+        data.append(entity)
+
     return jsonify(data), 200
 
-@app.route('/crear_duenio', methods=['POST'])
+@app.route('/create_duenio', methods=['POST'])
 def crear_duenio():
     conn = engine.connect()
-    new_owner = request.get_json()
-    query = text("""
-        INSERT INTO duenios (nombre, mail, telefono, barrio)
-        VALUES (:nombre, :mail, :telefono, :barrio);
-    """)
+    new_duenio = request.get_json()
+    query = f"""INSERT INTO duenios (nombre, mail, telefono, barrio) 
+                VALUES ('{new_duenio["nombre"]}', '{new_duenio["mail"]}', {new_duenio["telefono"]}, '{new_duenio["barrio"]}');"""
     try:
-        conn.execute(query, nombre=new_owner["nombre"], mail=new_owner["mail"], telefono=new_owner["telefono"], barrio=new_owner["barrio"])
+        conn.execute(text(query))
         conn.commit()
+        conn.close()
     except SQLAlchemyError as err:
         return jsonify({'message': 'Se ha producido un error: ' + str(err.__cause__)}), 500
-    finally:
-        conn.close()
+
     return jsonify({'message': 'Se ha agregado correctamente'}), 201
 
 @app.route('/duenios/<id_duenio>', methods=['PATCH'])
 def actualizar_duenio(id_duenio):
     conn = engine.connect()
-    mod_owner = request.get_json()
-    query = text("""
-        UPDATE duenios SET
-            nombre = :nombre,
-            mail = :mail,
-            telefono = :telefono,
-            barrio = :barrio
-        WHERE id_duenio = :id_duenio;
-    """)
-    query_validation = text("SELECT * FROM duenios WHERE id_duenio = :id_duenio;")
+    mod_duenio = request.get_json()
+    query = f"""UPDATE duenios SET nombre = '{mod_duenio['nombre']}'
+                {f", mail = '{mod_duenio['mail']}'" if "mail" in mod_duenio else ""}
+                {f", telefono = {mod_duenio['telefono']}" if "telefono" in mod_duenio else ""}
+                {f", barrio = '{mod_duenio['barrio']}'" if "barrio" in mod_duenio else ""}
+                WHERE id_duenio = {id_duenio};"""
+    query_validation = f"SELECT * FROM duenios WHERE id_duenio = {id_duenio};"
     try:
-        val_result = conn.execute(query_validation, id_duenio=id_duenio)
-        if val_result.rowcount == 0:
+        val_result = conn.execute(text(query_validation))
+        if val_result.rowcount != 0:
+            conn.execute(text(query))
+            conn.commit()
+            conn.close()
+        else:
+            conn.close()
             return jsonify({'message': "El dueño no existe"}), 404
-        conn.execute(query, id_duenio=id_duenio, nombre=mod_owner.get('nombre'), mail=mod_owner.get('mail'), telefono=mod_owner.get('telefono'), barrio=mod_owner.get('barrio'))
-        conn.commit()
     except SQLAlchemyError as err:
         return jsonify({'message': str(err.__cause__)}), 500
-    finally:
-        conn.close()
     return jsonify({'message': 'Se ha modificado correctamente'}), 200
 
 @app.route('/duenios/<id_duenio>', methods=['GET'])
 def obtener_duenio(id_duenio):
     conn = engine.connect()
-    query = text("SELECT * FROM duenios WHERE id_duenio = :id_duenio;")
+    query = f"SELECT * FROM duenios WHERE id_duenio = {id_duenio};"
     try:
-        result = conn.execute(query, id_duenio=id_duenio)
-        row = result.fetchone()
-        if row:
-            data = {
-                'id_duenio': row.id_duenio,
-                'nombre': row.nombre,
-                'mail': row.mail,
-                'telefono': row.telefono,
-                'barrio': row.barrio
-            }
-        else:
-            return jsonify({"message": "El dueño no existe"}), 404
+        result = conn.execute(text(query))
+        conn.commit()
+        conn.close()
     except SQLAlchemyError as err:
         return jsonify(str(err.__cause__)), 500
-    finally:
-        conn.close()
-    return jsonify(data), 200
+    if result.rowcount != 0:
+        row = result.first()
+        data = {
+            'id_duenio': row.id_duenio,
+            'nombre': row.nombre,
+            'mail': row.mail,
+            'telefono': row.telefono,
+            'barrio': row.barrio
+        }
+        return jsonify(data), 200
+    return jsonify({"message": "El dueño no existe"}), 404
 
 @app.route('/duenios/<id_duenio>', methods=['DELETE'])
 def borrar_duenio(id_duenio):
     conn = engine.connect()
-    query = text("DELETE FROM duenios WHERE id_duenio = :id_duenio;")
-    validation_query = text("SELECT * FROM duenios WHERE id_duenio = :id_duenio")
+    query = f"DELETE FROM duenios WHERE id_duenio = {id_duenio};"
+    validation_query = f"SELECT * FROM duenios WHERE id_duenio = {id_duenio};"
     try:
-        val_result = conn.execute(validation_query, id_duenio=id_duenio)
-        if val_result.rowcount == 0:
+        val_result = conn.execute(text(validation_query))
+        if val_result.rowcount != 0:
+            conn.execute(text(query))
+            conn.commit()
+            conn.close()
+        else:
+            conn.close()
             return jsonify({"message": "El dueño no existe"}), 404
-        conn.execute(query, id_duenio=id_duenio)
-        conn.commit()
     except SQLAlchemyError as err:
         return jsonify(str(err.__cause__)), 500
-    finally:
-        conn.close()
     return jsonify({'message': 'Se ha eliminado correctamente'}), 202
+
+if __name__ == "__main__":
+    app.run("127.0.0.1", port="5000", debug=True)
--- api_bdd/mascotas_perdidas_api.py
+++ api_bdd/mascotas_perdidas_api.py
@@ -4,8 +4,7 @@ from sqlalchemy import create_engine, text
 from sqlalchemy.exc import SQLAlchemyError
 
 app = Flask(__name__)
-engine = create_engine("mysql+mysqlconnector://root:12345@localhost/lost_pets_db")
-
+engine = create_engine("mysql+mysqlconnector://root@localhost/ids")
 
 @app.route('/mascotas_perdidas', methods=['GET'])
 def obtener_mascotas_perdidas():
@@ -13,118 +12,113 @@ def obtener_mascotas_perdidas():
     query = "SELECT * FROM mascotas_perdidas;"
     try:
         result = conn.execute(text(query))
-        data = []
-        for row in result:
-            entity = {
-                'id_mascota': row.id_mascota,
-                'raza': row.raza,
-                'nombre': row.nombre,
-                'color': row.color,
-                'sexo': row.sexo,
-                'edad_aprox': row.edad_aprox,
-                'tamanio': row.tamanio,
-                'barrio': row.barrio,
-                'mail_duenio': row.mail_duenio,
-                'telefono_duenio': row.telefono_duenio
-            }
-            data.append(entity)
+        conn.close()
     except SQLAlchemyError as err:
         return jsonify(str(err.__cause__)), 500
-    finally:
-        conn.close()
+
+    data = []
+    for row in result:
+        entity = {
+            'id_mascota': row.id_mascota,
+            'raza': row.raza,
+            'nombre': row.nombre,
+            'color': row.color,
+            'sexo': row.sexo,
+            'edad_aprox': row.edad_aprox,
+            'tamanio': row.tamanio,
+            'barrio': row.barrio,
+            'mail_duenio': row.mail_duenio,
+            'telefono_duenio': row.telefono_duenio
+        }
+        data.append(entity)
+
     return jsonify(data), 200
 
-@app.route('/crear_mascota_perdida', methods=['POST'])
-def crear_mascotas_perdidas():
+@app.route('/create_mascota_perdida', methods=['POST'])
+def crear_mascota_perdida():
     conn = engine.connect()
-    new_pet = request.get_json()
-    query = text("""
-        INSERT INTO mascotas_perdidas (raza, nombre, color, sexo, edad_aprox, tamanio, barrio, mail_duenio, telefono_duenio)
-        VALUES (:raza, :nombre, :color, :sexo, :edad_aprox, :tamanio, :barrio, :mail_duenio, :telefono_duenio);
-    """)
+    new_mascota = request.get_json()
+    query = f"""INSERT INTO mascotas_perdidas (raza, nombre, color, sexo, edad_aprox, tamanio, barrio, mail_duenio, telefono_duenio) 
+                VALUES ('{new_mascota["raza"]}', '{new_mascota["nombre"]}', '{new_mascota["color"]}', '{new_mascota["sexo"]}',
+                        '{new_mascota["edad_aprox"]}', '{new_mascota["tamanio"]}', '{new_mascota["barrio"]}',
+                        '{new_mascota["mail_duenio"]}', {new_mascota["telefono_duenio"]});"""
     try:
-        conn.execute(query, raza=new_pet["raza"], nombre=new_pet["nombre"], color=new_pet["color"], sexo=new_pet["sexo"], edad_aprox=new_pet["edad_aprox"], tamanio=new_pet["tamanio"], barrio=new_pet["barrio"], mail_duenio=new_pet["mail_duenio"], telefono_duenio=new_pet["telefono_duenio"])
+        conn.execute(text(query))
         conn.commit()
+        conn.close()
     except SQLAlchemyError as err:
         return jsonify({'message': 'Se ha producido un error: ' + str(err.__cause__)}), 500
-    finally:
-        conn.close()
+
     return jsonify({'message': 'Se ha agregado correctamente'}), 201
 
 @app.route('/mascotas_perdidas/<id_mascota>', methods=['PATCH'])
 def actualizar_mascota_perdida(id_mascota):
     conn = engine.connect()
-    mod_pet = request.get_json()
-    query = text("""
-        UPDATE mascotas_perdidas SET
-            raza = :raza,
-            nombre = :nombre,
-            color = :color,
-            sexo = :sexo,
-            edad_aprox = :edad_aprox,
-            tamanio = :tamanio,
-            barrio = :barrio,
-            mail_duenio = :mail_duenio,
-            telefono_duenio = :telefono_duenio
-        WHERE id_mascota = :id_mascota;
-    """)
-    query_validation = text("SELECT * FROM mascotas_perdidas WHERE id_mascota = :id_mascota;")
+    mod_mascota = request.get_json()
+    query = f"""UPDATE mascotas_perdidas SET raza = '{mod_mascota['raza']}', nombre = '{mod_mascota['nombre']}',
+                color = '{mod_mascota['color']}', sexo = '{mod_mascota['sexo']}', edad_aprox = '{mod_mascota['edad_aprox']}',
+                tamanio = '{mod_mascota['tamanio']}', barrio = '{mod_mascota['barrio']}', 
+                mail_duenio = '{mod_mascota['mail_duenio']}', telefono_duenio = {mod_mascota['telefono_duenio']}
+                WHERE id_mascota = {id_mascota};"""
+    query_validation = f"SELECT * FROM mascotas_perdidas WHERE id_mascota = {id_mascota};"
     try:
-        val_result = conn.execute(query_validation, id_mascota=id_mascota)
-        if val_result.rowcount == 0:
+        val_result = conn.execute(text(query_validation))
+        if val_result.rowcount != 0:
+            conn.execute(text(query))
+            conn.commit()
+            conn.close()
+        else:
+            conn.close()
             return jsonify({'message': "La mascota no existe"}), 404
-        conn.execute(query, id_mascota=id_mascota, raza=mod_pet.get('raza'), nombre=mod_pet.get('nombre'), color=mod_pet.get('color'), sexo=mod_pet.get('sexo'), edad_aprox=mod_pet.get('edad_aprox'), tamanio=mod_pet.get('tamanio'), barrio=mod_pet.get('barrio'), mail_duenio=mod_pet.get('mail_duenio'), telefono_duenio=mod_pet.get('telefono_duenio'))
-        conn.commit()
     except SQLAlchemyError as err:
         return jsonify({'message': str(err.__cause__)}), 500
-    finally:
-        conn.close()
     return jsonify({'message': 'Se ha modificado correctamente'}), 200
 
 @app.route('/mascotas_perdidas/<id_mascota>', methods=['GET'])
 def obtener_mascota_perdida(id_mascota):
     conn = engine.connect()
-    query = text("SELECT * FROM mascotas_perdidas WHERE id_mascota = :id_mascota;")
+    query = f"SELECT * FROM mascotas_perdidas WHERE id_mascota = {id_mascota};"
     try:
-        result = conn.execute(query, id_mascota=id_mascota)
-        row = result.fetchone()
-        if row:
-            data = {
-                'id_mascota': row.id_mascota,
-                'raza': row.raza,
-                'nombre': row.nombre,
-                'color': row.color,
-                'sexo': row.sexo,
-                'edad_aprox': row.edad_aprox,
-                'tamanio': row.tamanio,
-                'barrio': row.barrio,
-                'mail_duenio': row.mail_duenio,
-                'telefono_duenio': row.telefono_duenio
-            }
-        else:
-            return jsonify({"message": "La mascota no existe"}), 404
+        result = conn.execute(text(query))
+        conn.commit()
+        conn.close()
     except SQLAlchemyError as err:
         return jsonify(str(err.__cause__)), 500
-    finally:
-        conn.close()
-    return jsonify(data), 200
+    if result.rowcount != 0:
+        row = result.first()
+        data = {
+            'id_mascota': row.id_mascota,
+            'raza': row.raza,
+            'nombre': row.nombre,
+            'color': row.color,
+            'sexo': row.sexo,
+            'edad_aprox': row.edad_aprox,
+            'tamanio': row.tamanio,
+            'barrio': row.barrio,
+            'mail_duenio': row.mail_duenio,
+            'telefono_duenio': row.telefono_duenio
+        }
+        return jsonify(data), 200
+    return jsonify({"message": "La mascota no existe"}), 404
 
 @app.route('/mascotas_perdidas/<id_mascota>', methods=['DELETE'])
 def borrar_mascota_perdida(id_mascota):
     conn = engine.connect()
-    query = text("DELETE FROM mascotas_perdidas WHERE id_mascota = :id_mascota;")
-    validation_query = text("SELECT * FROM mascotas_perdidas WHERE id_mascota = :id_mascota")
+    query = f"DELETE FROM mascotas_perdidas WHERE id_mascota = {id_mascota};"
+    validation_query = f"SELECT * FROM mascotas_perdidas WHERE id_mascota = {id_mascota};"
     try:
-        val_result = conn.execute(validation_query, id_mascota=id_mascota)
-        if val_result.rowcount == 0:
+        val_result = conn.execute(text(validation_query))
+        if val_result.rowcount != 0:
+            conn.execute(text(query))
+            conn.commit()
+            conn.close()
+        else:
+            conn.close()
             return jsonify({"message": "La mascota no existe"}), 404
-        conn.execute(query, id_mascota=id_mascota)
-        conn.commit()
     except SQLAlchemyError as err:
         return jsonify(str(err.__cause__)), 500
-    finally:
-        conn.close()
     return jsonify({'message': 'Se ha eliminado correctamente'}), 202
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run("127.0.0.1", port="5000", debug=True)
+
