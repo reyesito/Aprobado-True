05d70c77d39d693855d0087f12fe43d42327cdc3
martin <lmsotelo@fi.uba.ar>
2024-06-07 14:16:18 -0300

    Tarea 14: correcion de tablas , actualizacion de templates y formularios inicio de Tarea 16

--- tpg_bdd/sql/init.sql
+++ tpg_bdd/sql/init.sql
@@ -1,51 +1,52 @@
-CREATE TABLE IF NOT EXISTS mascotas_encontradas (
-    id_mascota INT NOT NULL AUTO_INCREMENT,
-    raza VARCHAR(20),
-    nombre VARCHAR(20),
-    color VARCHAR(20),
-    sexo VARCHAR(3),
-    tamanio VARCHAR(10),
-    edad_aprox VARCHAR(10),
-    barrio VARCHAR(30),
-    mail_duenio VARCHAR(40),
-    telefono_duenio int(20),
-    telefono_informante int(20),
-    id_informante INT,
-    PRIMARY KEY(id_mascota),
-    FOREIGN KEY (telefono_informante) REFERENCES informante(telefono),
-    FOREIGN KEY (mail_duenio,telefono_duenio) REFERENCES duenios(mail,telefono)
-);
-CREATE TABLE IF NOT EXISTS mascotas_perdidas (
-    id_mascota INT NOT NULL AUTO_INCREMENT,
-    raza VARCHAR(20),
-    nombre VARCHAR(20),
-    color VARCHAR(20),
-    sexo VARCHAR(3),
-    edad_aprox VARCHAR(10),
-    tamanio VARCHAR(10),
-    barrio VARCHAR(30),
-    mail_duenio VARCHAR(40),
-    telefono_duenio int(20),
-    PRIMARY KEY(id_mascota),
-    FOREIGN KEY (mail_duenio,telefono_duenio) REFERENCES duenios(mail,telefono)
-
-);
-
 CREATE TABLE IF NOT EXISTS duenios (
     id_duenio INT NOT NULL AUTO_INCREMENT,
     nombre VARCHAR(50),
     mail VARCHAR(40),
-    telefono int(20),
+    telefono INT(20),
     barrio VARCHAR(50),
     PRIMARY KEY(id_duenio)
 );
 
-CREATE TABLE IF NOT EXISTS informante(
+CREATE TABLE IF NOT EXISTS informantes(
     id_informante INT NOT NULL AUTO_INCREMENT,
     nombre VARCHAR(50),
-    telefono int(20),
+    mail VARCHAR(40),
+    telefono INT(20),
     barrio VARCHAR(30),
     PRIMARY KEY(id_informante)
+);
+
+CREATE TABLE IF NOT EXISTS mascotas_encontradas (
+    id_mascota INT NOT NULL AUTO_INCREMENT,
+    animal VARCHAR(20),
+    raza VARCHAR(20),
+    nombre VARCHAR(20),
+    color VARCHAR(20),
+    sexo VARCHAR(3),
+    tamanio VARCHAR(10),
+    barrio VARCHAR(30),
+    mail_informante VARCHAR(40),
+    telefono_informante INT(20),
+    id_informante INT ,
+    PRIMARY KEY(id_mascota),
+    FOREIGN KEY (id_informante) REFERENCES informantes(id_informante)
+);
+
+CREATE TABLE IF NOT EXISTS mascotas_perdidas (
+    id_mascota INT NOT NULL AUTO_INCREMENT,
+    animal VARCHAR(20),
+    raza VARCHAR(20),
+    nombre VARCHAR(20),
+    color VARCHAR(20),
+    sexo VARCHAR(3),
+    tamanio VARCHAR(10),
+    barrio VARCHAR(30),
+    mail_duenio VARCHAR(40),
+    telefono_duenio INT(20),
+    id_duenio INT,
+    PRIMARY KEY(id_mascota),
+    FOREIGN KEY (id_duenio) REFERENCES duenios(id_duenio)
+
 );
 
 CREATE TABLE IF NOT EXISTS coordenadas (
