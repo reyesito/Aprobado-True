6bddc11b667f43225a47c1f5b082d2b152055d14
andypinta <andydjpro14@gmail.com>
2024-06-09 01:10:02 -0300

    Tarea #29 - Creacion de los templates para captar los errores 404 y 505, tambien agregue la ruta que capta el errorhandler en app.py

--- app.py
+++ app.py
@@ -159,5 +159,14 @@ def obtener_coordenadas():
     except Exception as e:
         return jsonify({'message': 'Se ha producido un error: ' + str(e)}), 500
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
 if __name__ == "__main__":
     app.run("127.0.0.1", port=5001, debug=True)
