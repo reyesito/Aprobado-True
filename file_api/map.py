00da3164325cdadb0d95a2f73cf37e84adb03236
Tarkus <lmsotelo@fi.uba.ar>
2024-06-17 22:23:12 -0300

    mapa interactivo finalizado

--- file_api/map.py
+++ file_api/map.py
@@ -4,16 +4,20 @@ from sqlalchemy.exc import SQLAlchemyError
 
 engine = create_engine("mysql+mysqlconnector://root:12345@localhost:3308/lost_pets_db")
 
-#Funcion de Mapa de coordenadas
 def obtener_coordenadas():
     conn = engine.connect()
-    query = "SELECT latitud, altitud FROM coordenadas;"
+    query = """
+        SELECT latitud, altitud, 'coordenadas' AS tipo FROM coordenadas
+        UNION
+        SELECT latitud, altitud, 'mascotas_encontradas' AS tipo FROM mascotas_encontradas
+        UNION
+        SELECT latitud, altitud, 'mascotas_perdidas' AS tipo FROM mascotas_perdidas
+    """
     try:
         result = conn.execute(text(query))
+        data = [{'latitud': row[0], 'altitud': row[1], 'tipo': row[2]} for row in result]
         conn.close()
+        return data, 200
     except Exception as e:
         return None, 500
 
-    data = [{'latitud': row[0], 'altitud': row[1]} for row in result]
-
-    return data, 200
